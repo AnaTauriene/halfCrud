560eed6da86447d9544d166935d849625f316812
anata <ana.tauriene@gmail.com>
2018-12-13 12:26:45 +0200

    cleanUp

--- static/JS/table.js
+++ static/JS/table.js
@@ -1,13 +1,12 @@
 function showTable(){
-   let element = document.getElementById('hiddenTable')
-   element.classList.remove("visibleTable");
+   document.getElementById('hiddenTable').classList.remove("visibleTable");
 }
 
 function validate() {
    const uname = document.getElementById("name");
    const usurname = document.getElementById("surname");
-   let upass = document.getElementById("password");
-   let uemail = document.getElementById("email");
+   const upass = document.getElementById("password");
+   const uemail = document.getElementById("email");
       if (passValid(upass, 7,12)) {
          if (allLetter(uname, usurname)) {
             if (validEmail(uemail)) {
@@ -20,7 +19,7 @@ function validate() {
 }
 
 function passValid(upass,mx,my){
-const upass_len = upass.value.length;
+   const upass_len = upass.value.length;
    if (upass_len == 0 ||upass_len >= my || upass_len < mx){
       alert("Password should not be empty / length be between "+mx+" to "+my);
       upass.focus();
@@ -29,22 +28,22 @@ const upass_len = upass.value.length;
 return true;
 }
 
-function allLetter(uname, usurname){ 
-const letters = /^[A-Za-z]+$/;
-   if(uname.value.match(letters)){
-   return true;
-   }else{
-   alert('Username must have alphabet characters only');
-   uname.focus();
-   return false;
+function allLetter(uname, usurname) { 
+   const letters = /^[A-Za-z]+$/;
+   if (uname.value.match(letters)) {
+      return true;
+   } else {
+      alert('Username must have alphabet characters only');
+      uname.focus();
+      return false;
    }
 }
 
-function validEmail(uemail){
-const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-   if(uemail.value.match(mailformat)){
+function validEmail(uemail) {
+   const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+   if (uemail.value.match(mailformat)) {
       return true;
-   }else{
+   } else {
       alert("You have entered an invalid email address!");
       uemail.focus();
       return false;
